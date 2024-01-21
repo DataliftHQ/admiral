422726ce33b2f04df826bde19bd1e94b509bdcf4
Martin H Berwanger <mberwanger@protonmail.com>
2024-01-20 21:45:02 -0500

    add tools

---
+++ tools/golangci-lint.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+REPO_ROOT="${REPO_ROOT:-"$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")"}"
+BUILD_ROOT="${REPO_ROOT}/build"
+BUILD_BIN="${BUILD_ROOT}/bin"
+
+NAME=golangci-lint
+RELEASE=v1.55.2
+OSX_RELEASE_256=632e96e6d5294fbbe7b2c410a49c8fa01c60712a0af85a567de85bcc1623ea21
+LINUX_RELEASE_256=ca21c961a33be3bc15e4292dc40c98c8dcc5463a7b6768a3afc123761630c09c
+
+ARCH=amd64
+
+RELEASE_BINARY="${BUILD_BIN}/${NAME}-${RELEASE}"
+
+main() {
+  ensure_binary
+
+  "${RELEASE_BINARY}" "$@"
+}
+
+ensure_binary() {
+  if [[ ! -f "${RELEASE_BINARY}" ]]; then
+    echo "info: Downloading ${NAME} ${RELEASE} to build environment"
+
+    mkdir -p "${BUILD_BIN}"
+
+    case "${OSTYPE}" in
+      "darwin"*) os_type="darwin"; sum="${OSX_RELEASE_256}" ;;
+      "linux"*) os_type="linux"; sum="${LINUX_RELEASE_256}" ;;
+      *) echo "error: Unsupported OS '${OSTYPE}' for shellcheck install, please install manually" && exit 1 ;;
+    esac
+
+    release_archive="/tmp/${NAME}-${RELEASE}.tar.gz"
+
+    URL="https://github.com/golangci/golangci-lint/releases/download/${RELEASE}/golangci-lint-${RELEASE:1}-${os_type}-${ARCH}.tar.gz"
+    curl -sSL -o "${release_archive}" "${URL}"
+    echo "${sum}" ${release_archive} | sha256sum --check --quiet -
+
+    release_tmp_dir="/tmp/${NAME}-${RELEASE}"
+    mkdir -p "${release_tmp_dir}"
+    tar -xzf "${release_archive}" --strip=1 -C "${release_tmp_dir}"
+
+    if [[ ! -f "${RELEASE_BINARY}" ]]; then
+      find "${BUILD_BIN}" -maxdepth 0 -regex '.*/'${NAME}'-[A-Za-z0-9\.]+$' -exec rm {} \;  # cleanup older versions
+      mv "${release_tmp_dir}/${NAME}" "${RELEASE_BINARY}"
+    fi
+
+    # Cleanup stale resources.
+    rm "${release_archive}"
+    rm -rf "${release_tmp_dir}"
+  fi
+}
+
+main "$@"
