1930faeb2169498d380e53d9eda3fbe68cb6e1c2
Martin H Berwanger <mberwanger@protonmail.com>
2024-01-25 12:07:39 -0500

    add header and tls

---
+++ common/client/tls.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"crypto/tls"
+	"fmt"
+
+	tls_util "go.datalift.io/datalift/common/util/tls"
+)
+
+func (c *client) tlsConfig() (*tls.Config, error) {
+	var tlsConfig tls.Config
+	if len(c.CertPEMData) > 0 {
+		cp := tls_util.BestEffortSystemCertPool()
+		if !cp.AppendCertsFromPEM(c.CertPEMData) {
+			return nil, fmt.Errorf("credentials: failed to append certificates")
+		}
+		tlsConfig.RootCAs = cp
+	}
+	if c.ClientCert != nil {
+		tlsConfig.Certificates = append(tlsConfig.Certificates, *c.ClientCert)
+	}
+	if c.Insecure {
+		tlsConfig.InsecureSkipVerify = true
+	}
+	return &tlsConfig, nil
+}
