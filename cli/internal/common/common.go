1fd1c4be151c1f9e25c438ee422d6b809dd3cb87
Martin H Berwanger <mberwanger@protonmail.com>
2024-01-25 15:09:34 -0500

    add cli skeleton

---
+++ cli/internal/common/common.go
@@ -0,0 +1,20 @@
+package common
+
+import "github.com/coreos/go-oidc/v3/oidc"
+
+const (
+	EnvLogFormat      = "DATALIFT_LOG_FORMAT"
+	EnvLogLevel       = "DATALIFT_LOG_LEVEL"
+	EnvServerAddress  = "DATALIFT_SERVER_ADDRESS"
+	EnvOAuth2Issuer   = "DATALIFT_OAUTH2_ISSUER"
+	EnvOAuth2ClientId = "DATALIFT_OAUTH2_CLIENT_ID"
+	EnvOAuth2Scopes   = "DATALIFT_OAUTH2_SCOPES"
+
+	DefaultServerAddress  = "api.datalift.io:443"
+	DefaultOAuth2Issuer   = "https://auth.datalift.io"
+	DefaultOAuth2ClientId = "44972cb5-9739-4b8d-ac29-6dcccca3e9db"
+)
+
+var (
+	DefaultOAuth2Scopes = []string{oidc.ScopeOpenID, oidc.ScopeOfflineAccess}
+)
---
+++ cli/internal/config/config.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"gopkg.in/yaml.v2"
+	"io"
+	"os"
+	"path"
+	"runtime"
+	"sync"
+)
+
+var saveMutex sync.Mutex
+
+type Config struct {
+	Version       int    `yaml:"version,omitempty" json:"version,omitempty"`
+	ServerAddress string `yaml:"server_address,omitempty" json:"server_address,omitempty"`
+	OAuth2        OAuth2 `yaml:"oauth2,omitempty" json:"oauth2,omitempty"`
+}
+
+type OAuth2 struct {
+	Issuer   string   `yaml:"issuer,omitempty" json:"issuer,omitempty"`
+	ClientId string   `yaml:"client_id,omitempty" json:"client_id,omitempty"`
+	Scopes   []string `yaml:"scopes,omitempty" json:"scopes,omitempty"`
+}
+
+func Load(file string) (config Config, err error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	return LoadReader(f)
+}
+
+func LoadReader(fd io.Reader) (config Config, err error) {
+	data, err := io.ReadAll(fd)
+	if err != nil {
+		return config, err
+	}
+
+	err = yaml.UnmarshalStrict(data, &config)
+	return config, err
+}
+
+func Save(file string, config Config) error {
+	saveMutex.Lock()
+	defer saveMutex.Unlock()
+
+	data, err := yaml.Marshal(&config)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(file, data, 0600)
+}
+
+func ConfigDir() (string, error) {
+	if configDir := os.Getenv("DATALIFT_CONFIG_DIR"); configDir != "" {
+		return configDir, nil
+	}
+
+	if xdgConfigHome := os.Getenv("XDG_CONFIG_HOME"); xdgConfigHome != "" {
+		return path.Join(xdgConfigHome, "datalift"), nil
+	}
+
+	if winConfigHome := os.Getenv("AppData"); winConfigHome != "" && runtime.GOOS == "windows" {
+		return path.Join(winConfigHome, "Datalift"), nil
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	return path.Join(homeDir, ".config", "datalift"), nil
+}
